57eefe5ee94d245bb7ac45f0bfbfe6241ce7b848
hisarciklilar <hisarciklilar@gmail.com>
2024-11-28 01:11:44 +0000

    Feat: Add quiz question dictionary list

---
+++ question_list.py
@@ -0,0 +1,14 @@
+question_list =[
+    {"question": "Serial Correlation is a common problem in time-series data", "answer": "True"}, 
+    {"question": "Standard error is obtained through dividing the standard deviation by squareroot of the sample size", "answer": "True"}, 
+    {"question": "Serial correlation is the same as autocorrelation", "answer": "True"}, 
+    {"question": "Logit is used when we have a binary dependent variable", "answer": "True"}, 
+    {"question": "In OLS, the direction of causality is from the independent variable to the dependent variable", "answer": "True"}, 
+    {"question": "Independent and explanatory variables refer to the same thing", "answer": "True"}, 
+    {"question": "In an autoregresive model, the lagged dependent variable is included in the independent variable list", "answer": "True"}, 
+    {"question": "In statstical testing, null hypothesis is rejected when p-value is less than significance level", "answer": "True"}, 
+    {"question": "One minus signifiance level gives the confidence level", "answer": "True"}, 
+    {"question": "Two-stage Least Squares is another name used for Instrumental Variable Estimation", "answer": "True"}, 
+    {"question": "Logit and Probit model coefficient estimates are obtained by Maximum Likelihood estimation", "answer": "True"}, 
+]
+
--- run.py
+++ run.py
@@ -1,16 +1,25 @@
-import gspread
-from google.oauth2.service_account import Credentials
+# import gspread
+# from google.oauth2.service_account import Credentials
 
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
+from question_list import question_list
 
-CREDS = Credentials.from_service_account_file("creds.json")
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open("quants_dojo")
+for i in range(len(question_list)):
+    print(question_list[i])
+
+for i in range(len(question_list)):
+    print(question_list[i]["question"])
+
+
+# SCOPE = [
+#     "https://www.googleapis.com/auth/spreadsheets",
+#     "https://www.googleapis.com/auth/drive.file",
+#     "https://www.googleapis.com/auth/drive"
+#     ]
+
+# CREDS = Credentials.from_service_account_file("creds.json")
+# SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+# GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+# SHEET = GSPREAD_CLIENT.open("quants_dojo")
 
 # quiz_response = SHEET.worksheet("quiz_response")
 # data = quiz_response.get_all_values()
