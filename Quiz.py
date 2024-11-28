48a9b2b927313d8985936d0022905333a2df2d16
Mehtap Hisarciklilar <hisarciklilar@gmail.com>
2024-11-28 17:22:24 +0000

    Feat: Calculate score

--- Quiz.py
+++ Quiz.py
@@ -3,7 +3,8 @@ class Quiz:
     def __init__(self, question_list):
         self.question_index = 0
         self.question_list = question_list
-        self.response =""
+        self.response = ""
+        self.score = 0 
 
 
     def end_of_quiz(self):
@@ -14,17 +15,20 @@ class Quiz:
 
     def reveal_question(self):
         question_to_reveal = self.question_list[self.question_index]["question"]
-        print(f"Q.{self.question_index + 1}: True or False?\n")
-        self.response = input(f"'{question_to_reveal}' (True/False)\n")
+        print(f"Q.{self.question_index + 1}: True or False?\n \n")
+        self.response = input(f"'{question_to_reveal}' (True/False)\n \n")
         self.check_answer()
         self.question_index += 1
 
+
     def check_answer(self):
         if self.response.lower() == self.question_list[self.question_index]["answer"].lower():
+            self.score += 1
             print(f"Correct! \n \n  The answer was {self.question_list[self.question_index]["answer"]}\n \n")
         elif self.response.lower() == "quit":
             print("You chose to quit the session.")
         else:
             print(f"This is incorrect.\n \n  The answer was {self.question_list[self.question_index]["answer"]}\n \n")
-    
+        print(f"You have answered {self.score} out of {self.question_index +1} questions correctly. \n \n")
+        print(f"Your current score is {round((self.score / (self.question_index + 1))*100, 2)}% \n \n")    
         
