6be3333599435d8bfba7dd7e00883229eacb5dec
Mehtap Hisarciklilar <hisarciklilar@gmail.com>
2024-11-29 16:02:58 +0000

    Feat: Modify welcome screen

---
+++ quiz/quiz_start.py
@@ -0,0 +1,22 @@
+import os
+import quiz.quiz
+import quiz.assets.logo as logo
+from quiz.assets.question_bank import question_list
+import quiz.quiz_generator
+
+def start_quiz():
+    print(logo.logo_picture)
+    print("Welcome to Quants Dojo!\n")
+    print("Ready to test your Econometrics skills? \n")
+    input("Press enter if you dare!\n")
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def quiz_info():
+    print(logo.logo_title)
+    print("This challenge includes 10 True/False questions.\n")
+    print("Your task is to find the correct answer and type either 'true' or 'false' in terminal.\n")
+    print("You may type 'quit' to quit the quiz anytime.\n")
+
+def quiz_start():
+    start_quiz()
+    quiz_info()
--- run.py
+++ run.py
@@ -1,27 +1,19 @@
 # import gspread
 # from google.oauth2.service_account import Credentials
-
+import os
 import quiz.quiz
 import quiz.assets.logo as logo
-from quiz.assets.question_bank import question_list as question_list
+from quiz.assets.question_bank import question_list
 import quiz.quiz_generator
-
-
-def start_quiz():
-    print("Welcome to Quants Dojo!\n\n")
-    print("This is a place where you can test your Econometrics skills \n \n")
-    print("Ready for the challenge?! \n\n")
-
-
-print(logo.logo_title)
-print(logo.logo_picture)
+import quiz.quiz_start as quiz_start
 
 quiz_generator = quiz.quiz_generator.QuizGenerator(question_list,3)
 quiz_question_list = quiz_generator.generate_quiz()
 
 quiz = quiz.quiz.Quiz(quiz_question_list)
 
-start_quiz()
+quiz_start.quiz_start()
+
 while not quiz.end_of_quiz():
     quiz.reveal_question()
     
