f2517882bb2f6e1c9f628b755389bdfce41c7868
Mehtap Hisarciklilar <hisarciklilar@gmail.com>
2024-11-29 20:07:32 +0000

    Refactor: Rearrange quiz_start.py

--- quiz/quiz_start.py
+++ quiz/quiz_start.py
@@ -4,12 +4,15 @@ import quiz.assets.logo as logo
 from quiz.assets.question_bank import question_list
 import quiz.quiz_generator
 
+def clear_terminal_start():
+    input("Press enter if you dare!\n")
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 def start_quiz():
     print(logo.logo_picture)
     print("Welcome to Quants Dojo!\n")
     print("Ready to test your Econometrics skills? \n")
-    input("Press enter if you dare!\n")
-    os.system('cls' if os.name == 'nt' else 'clear')
+    clear_terminal_start()
 
 def quiz_info():
     print(logo.logo_title)
