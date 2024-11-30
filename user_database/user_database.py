1c30493fab286f2977e59fc386393ecfcfcb6aa6
Mehtap Hisarciklilar <hisarciklilar@gmail.com>
2024-11-30 22:17:11 +0000

    Docs: Add function descriptions to UserDatabase class

--- user_database/user_database.py
+++ user_database/user_database.py
@@ -1,7 +1,8 @@
 import gspread
 from google.oauth2.service_account import Credentials
 import datetime
-# from run import QUIZ_LENGTH
+
+
 QUIZ_LENGTH =10 
 
 SCOPE = [
@@ -26,6 +27,9 @@ class UserDatabase:
     
     
     def call_user_id_list(self):
+        """
+        Returns user id list from owner's user database spreadsheet
+        """
         user_id_list_str = SHEET.worksheet("user_list").col_values(1)
         user_id_list_str.pop(0)
         user_id_list = [int(id) for id in user_id_list_str]
@@ -33,65 +37,73 @@ class UserDatabase:
 
         
     def calculate_final_score(self):
+        """
+        Wraps user's correct/incorrect responses in a list that is used to calculate quiz score
+        """
         quiz_response_sheet = SHEET.worksheet("quiz_response")
-        if len(self.score_list) == QUIZ_LENGTH:
-            print("You answered all questions")
-        else:
+        if len(self.score_list) != QUIZ_LENGTH:
             print("You did not answer all questions")
             missing_responses = QUIZ_LENGTH - len(self.score_list)
-        for i in range(missing_responses):
-            self.score_list.append(0)
-        print(self.score_list)
-        calculate_quiz_score(self)
+            for i in range(missing_responses):
+                self.score_list.append(0)
+        self.calculate_quiz_score()
         self.score_list.insert(0, self.user_id)
         self.score_list.append(self.quiz_score)
-        add_date_to_quiz_record(self)
-        print(self.score_list)
-        call_previous_score(self)
+        self.add_date_to_quiz_record()
+        self.call_previous_score()
         quiz_response_sheet.append_row(self.score_list)
 
-def calculate_quiz_score(self):
-    self.quiz_score = (sum(self.score_list) / QUIZ_LENGTH) * 100
 
-def add_date_to_quiz_record(self):
-    date_time = datetime.datetime.now().strftime("%c")
-    self.score_list.append(date_time)
+    def calculate_quiz_score(self):
+        """ 
+        Calculates user's quiz score based on a list of responses
+        """
+        self.quiz_score = (sum(self.score_list) / QUIZ_LENGTH) * 100
 
-def call_previous_score(self):
-    quiz_score_list_str = SHEET.worksheet("quiz_response").col_values(12)   
-    print(quiz_score_list_str)
-    recorded_user_id_list_str = SHEET.worksheet("quiz_response").col_values(1) 
-    print(recorded_user_id_list_str)
-    date_time = SHEET.worksheet("quiz_response").col_values(13)   
-    print(date_time)
-    quiz_score_list_str.pop(0)
-    print(quiz_score_list_str)
-    recorded_user_id_list_str.pop(0)
-    date_time.pop(0)
-    quiz_score_list = [int(score) for score in quiz_score_list_str]
-    recorded_user_id_list = [int(id) for id in recorded_user_id_list_str]
-    print(quiz_score_list)
-    print(recorded_user_id_list)
-    if self.user_id in recorded_user_id_list:
-        print("you took this quiz before")
-        recorded_user_id_list.reverse()
-        quiz_score_list.reverse()
-        date_time.reverse()
-        index = recorded_user_id_list.index(self.user_id)
-        print(index)
-        self.previous_score = quiz_score_list[index]
-        previous_date_time = date_time[index]
-        print(f"You scored {self.quiz_score}% on this attempt.\n")
-        print(f"Your previous score on this test was {self.previous_score}% on {previous_date_time}\n")
-        provide_feedback(self)
-    else:
-        print(f"You scored {self.quiz_score}% on this quiz.\n")
-        print("come back to increase your test score!")
 
-def provide_feedback(self):
-    if self.quiz_score > self.previous_score:
-        print("Your score has increased in comparison to previous time! Well done on your improvement!\n")
-    if self.quiz_score == self.previous_score:
-        print("Your score has remained the same in comparison to last time.\n")
-    if self.quiz_score < self.previous_score:
-        print("Your score has decreased since last time. You may want to take the quiz again.\n")
+    def add_date_to_quiz_record(self):
+        """
+        Adds date-time stamp to user's score list
+        """
+        date_time = datetime.datetime.now().strftime("%c")
+        self.score_list.append(date_time)
+
+
+    def call_previous_score(self):
+        """
+        Checks user's latest previous score and provides feedback on performance
+        """
+        quiz_score_list_str = SHEET.worksheet("quiz_response").col_values(12)   
+        recorded_user_id_list_str = SHEET.worksheet("quiz_response").col_values(1) 
+        date_time = SHEET.worksheet("quiz_response").col_values(13)   
+        quiz_score_list_str.pop(0)
+        recorded_user_id_list_str.pop(0)
+        date_time.pop(0)
+        quiz_score_list = [int(score) for score in quiz_score_list_str]
+        recorded_user_id_list = [int(id) for id in recorded_user_id_list_str]
+        if self.user_id in recorded_user_id_list:
+            recorded_user_id_list.reverse()
+            quiz_score_list.reverse()
+            date_time.reverse()
+            index = recorded_user_id_list.index(self.user_id)
+            print(index)
+            self.previous_score = quiz_score_list[index]
+            previous_date_time = date_time[index]
+            print(f"You scored {self.quiz_score}% on this attempt.\n")
+            print(f"Your previous score on this test was {self.previous_score}% on {previous_date_time}\n")
+            self.provide_feedback()
+        else:
+            print(f"You scored {self.quiz_score}% on this quiz.\n")
+            print("come back to increase your test score!")
+
+
+    def provide_feedback(self):
+        """ 
+        Provides feedback based on a comparison of previous and current scores
+        """
+        if self.quiz_score > self.previous_score:
+            print("Your score has increased in comparison to previous time! Well done on your improvement!\n")
+        if self.quiz_score == self.previous_score:
+            print("Your score has remained the same in comparison to last time.\n")
+        if self.quiz_score < self.previous_score:
+            print("Your score has decreased since last time. You may want to take the quiz again.\n")
